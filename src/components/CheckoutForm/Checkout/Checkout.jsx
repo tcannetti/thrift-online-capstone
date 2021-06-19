85d3ad9db8dac51ced9b37c8271e0811174e4fd5
tcannetti <tcannetti@gmail.com>
2021-06-19 15:09:41 -0500

    created components for different steps in the checkout process

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+
 import useStyles from './checkoutStyles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 
 const steps = ['Shipping address', 'Payment details'];
@@ -8,6 +11,14 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
   const classes = useStyles();
+
+  const Confirmation = () => (
+    <div>Confirmation</div>
+  )
+
+  const Form = () => activeStep === 0
+    ? <AddressForm />
+    : <PaymentForm />
 
   return (
     <>
@@ -22,6 +33,7 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
+          { activeStep === steps.length ? <Confirmation /> : <Form /> }
         </Paper>
       </main>
     </>
