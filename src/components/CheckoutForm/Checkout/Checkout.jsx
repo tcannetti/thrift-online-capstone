c7b24de1415f44d5e3709a13647e2c3579e4b962
tcannetti <tcannetti@gmail.com>
2021-06-19 15:03:01 -0500

    steps now showing up

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -3,24 +3,24 @@ import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider,
 import useStyles from './checkoutStyles';
 
 
-const steps = ['Shipping Address', 'Payment Details'];
+const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
-  const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
 
   return (
     <>
       <div className={classes.toolbar}/>
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Typography variant="h4" align="center"></Typography>
-          <Stepper activeStep={0} className={classes.stepper}>
-            {steps.map((step) => {
+          <Typography variant="h4" align="center">Checkout</Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
               <Step key={step}>
-                <StepLabel key={step}></StepLabel>
+                <StepLabel>{step}</StepLabel>
               </Step>
-            })}
+            ))}
           </Stepper>
         </Paper>
       </main>
