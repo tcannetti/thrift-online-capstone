b8fd3b141c31b5b560be94fb2cacd2860291402f
tcannetti <tcannetti@gmail.com>
2021-06-21 12:14:48 -0500

    address form inputs

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { InputLabel, SelectMenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from './CustomTextField';
 
 const AddressForm = () => {
+  const methods = useForm();
+
   return (
-    <div>
-      Address Form
-    </div>
+    <>
+      <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSumbit=''>
+          <Grid container spacing={3}>
+            <FormInput required label="First Name" name="firstName" />
+            <FormInput required label="Last Name" name="lastName" />
+            <FormInput required label="Address" name="address1" />
+            <FormInput required label="Email" name="email" />
+            <FormInput required label="City" name="city" />
+            <FormInput required label="Zip" name="zip" />
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Country
+                </MenuItem>
+              </Select>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Subdivision
+                </MenuItem>
+              </Select>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Option
+                </MenuItem>
+              </Select>
+            </Grid>
+
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
   )
 }
 
---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { TextField, Grid } from '@material-ui/core';
+import { useFormContext, Controller } from 'react-hook-form';
+
+
+const FormInput = ({ name, label, required}) => {
+const { control } = useFormContext();
+   const isError = false;
+
+return (
+   <>
+         <Controller
+            control={control}
+            name={name}
+            render = {({ field})=> (
+                <TextField
+                    fullWidth
+                    label={label}
+                    required
+                />
+            )}
+         />
+   </>
+ );
+ }
+
+export default FormInput;
+
+// import React from 'react'
+// import { TextField, Grid } from '@material-ui/core';
+// import { useFormContext, Controller } from 'react-hook-form';
+
+// const FormInput = ({ name, label, required }) => {
+//   const { control } = useFormContext();
+
+//   return (
+//     <Grid item xs={12} sm={6}>
+//       <Controller 
+//         as={TextField}
+//         control={control}
+//         fullWidth
+//         name={name}
+//         label={label}
+//         required={required}
+//       />
+//     </Grid>
+//   )
+// }
+
+// export default FormInput
