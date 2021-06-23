2853a1fcfa2ec7f8347f95e219682b34de8b1ccc
tcannetti <tcannetti@gmail.com>
2021-06-23 11:24:47 -0500

    fixed buttons

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
- 
+import { Link } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
 
-const AddressForm = ({ checkoutToken }) => {
+const AddressForm = ({ checkoutToken, next }) => {
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState('');
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -58,7 +58,7 @@ const AddressForm = ({ checkoutToken }) => {
     <>
       <Typography variant="h6" gutterBottom>Shipping Address</Typography>
       <FormProvider {...methods}>
-        <form onSumbit=''>
+        <form onSumbit={methods.handleSubmit((data) => next({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
           <Grid container spacing={3}>
             <FormInput required label="First Name" name="firstName" />
             <FormInput required label="Last Name" name="lastName" />
@@ -101,6 +101,11 @@ const AddressForm = ({ checkoutToken }) => {
             </Grid>
 
           </Grid>
+          <br />
+          <div style={{ display: 'flex', justifyContent:"space-between" }}>
+            <Button component={Link} to="/cart" variant="outlined">Back to Cart</Button>
+            <Button type="submit" variant="contained" color="primary">Next</Button>
+          </div>
         </form>
       </FormProvider>
     </>
